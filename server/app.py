c6be0bccd19dbc5ce82a34153b4fc105f094aae0
Jonathan MacDuff <jonathan.macduff@outlook.com>
2024-08-03 15:53:14 -0700

    completed lab

--- server/app.py
+++ server/app.py
@@ -8,19 +8,68 @@ from config import app, db, api
 from models import User, Recipe
 
 class Signup(Resource):
-    pass
+    def post(self):
+        json = request.get_json()
+        username = json.get('username')
+        password = json.get('password')
+        image_url = json.get('image_url')
+        bio = json.get('bio')
+        if not all([username, password, image_url, bio]):
+            return {'message': 'All fields must be filled'}, 422
+        new_user = User(username=username, image_url=image_url, bio=bio)
+        new_user.password_hash = password
+        db.session.add(new_user)
+        db.session.commit()
+        new_user_dict = new_user.to_dict()
+        return new_user_dict, 201
+
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        if session['user_id']:
+            user = User.query.filter(User.id == session['user_id']).first()
+            return user.to_dict(), 200
+        return {'message': 'Please log in to continue'}, 401
 
 class Login(Resource):
-    pass
+    def post(self):
+        json = request.get_json()
+        username = json.get('username')
+        password = json.get('password')
+        user = User.query.filter(User.username == username).first()
+        if not isinstance(user, User) or not user.authenticate(password):
+            return {'error': 'Incorrect username or password'}, 401
+        session['user_id'] = user.id
+        return user.to_dict(), 200
+
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        if session['user_id']:
+            session['user_id'] = ''
+            return {'message': 'Logged out successfully'}, 200
+        return {'message': 'Not yet logged in'}, 401
 
 class RecipeIndex(Resource):
-    pass
+    def get(self):
+        user = User.query.filter(User.id == session['user_id']).first()
+        if user:
+            recipes = [recipe.to_dict() for recipe in user.recipes]
+            return recipes, 200
+        return {'message': 'Please log in to continue'}, 401
+    def post(self):
+        json = request.get_json()
+        title = json.get('title')
+        instructions = json.get('instructions')
+        minutes_to_complete = json.get('minutes_to_complete')
+        if not session['user_id']:
+            return {'error': 'Please log in to continue'}, 401
+        if all([title, len(instructions)>=50, minutes_to_complete]):
+            recipe = Recipe(title=title, instructions=instructions, minutes_to_complete=minutes_to_complete, user_id=session['user_id'])
+            db.session.add(recipe)
+            db.session.commit()
+            return recipe.to_dict(), 201
+        return {'error': 'Recipe invalid'}, 422
 
 api.add_resource(Signup, '/signup', endpoint='signup')
 api.add_resource(CheckSession, '/check_session', endpoint='check_session')
